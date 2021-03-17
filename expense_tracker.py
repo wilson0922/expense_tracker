5e94fa609feac88057e3bbf8e3039f26c328b64a
wilson0922 <tree70373@gmail.com>
2021-03-17 15:53:35 +0800

    refactor

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,35 +1,50 @@
 import os
 
-#判斷是否有檔案且讀取
-products = []
-if os.path.isfile('products.csv'):
-	print('檔案載入')
-	with open('products.csv', 'r', encoding='utf-8')as f:
+#讀取檔案
+def read_file(filename):
+	products = []
+	with open(filename, 'r', encoding='utf-8')as f:
 		for line in f:
 			if '商品, 價格' in line:
 				continue
 			name, price = line.strip().split(',')
 			products.append([name, price])
-else:
-	print('找不到檔案，請輸入商品')
+	return products
 
 #輸入商品
-while True:
-	name = input('請輸入商品名稱 :')
-	if name == 'q' or name =='Q':
-		print('程式結束')
-		break
-	price = input('請輸入商品價格: ')
-	price = int(price)
-	products.append([name, price])
+def user_input(products):
+	while True:
+		name = input('請輸入商品名稱 :')
+		if name == 'q' or name =='Q':
+			print('程式結束')
+			break
+		price = input('請輸入商品價格: ')
+		price = int(price)
+		products.append([name, price])
+	return products
 
 #印出所有商品價格
-for p in products:
-	print(p[0],'的價格是', p[1])
+def print_products(products):
+	for p in products:
+		print(p[0],'的價格是', p[1])
 
 #寫入商品
-with open('products.csv', 'w', encoding= 'utf-8')as f:
-	f.write('商品, 價格\n')
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+def write_file(filename, products):
+	with open(filename, 'w', encoding= 'utf-8')as f:
+		f.write('商品, 價格\n')
+		for p in products:
+			f.write(p[0] + ',' + str(p[1]) + '\n')
 
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename):
+		print('檔案載入')
+		products = read_file(filename)
+	else:
+		print('找不到檔案，請輸入商品')
+
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
+
+main()
